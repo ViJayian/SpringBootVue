086e15eb15d6807cf287c237e074014ec7a86e03
ViJayian <13716689854@163.com>
2021-12-03 22:11:03 +0800

    maven profiles

--- 01SpringBootProperties/src/main/java/org/vijayian/Application.java
+++ 01SpringBootProperties/src/main/java/org/vijayian/Application.java
@@ -3,6 +3,12 @@ package org.vijayian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * root.
@@ -10,6 +16,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @author ViJay
  */
 @SpringBootApplication
+@RestController //is ok
+//@Component //not ok
 public class Application {
     public static void main(String[] args) {
         //>> TODO 设置启动配置文件.
@@ -17,6 +25,15 @@ public class Application {
         SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
         builder.application().setAdditionalProfiles("dev");
         builder.run(args);
+        ConfigurableApplicationContext context = builder.context();
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        System.out.println(beanDefinitionNames);
 //        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/req")
+//    @ResponseBody
+    public String req() {
+        return "ok";
     }
 }
